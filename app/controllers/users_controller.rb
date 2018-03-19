161bef5c4d2960802403921ee5cd11d8193f1f36
Sally Zasloff <szasloff@gmail.com>
2018-03-19 21:52:14 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
   use Rack::Flash
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
+
+  get '/signup' do
+    if !logged_in?
+      erb :'/users/create_user'
+    else
+      redirect '/credits'
+    end
+  end
+
+
+  post '/signup' do
+
+    if params[:username] == "" || params[:password] == ""
+      flash[:message] = "Missing Information - Please Try Again"
+      redirect '/signup'
+    else
+      @user = User.new(username: params[:username]), password: params[:password])
+      @user.save
+
+      session[:user_id] = @user.id
+      redirect '/credits'
+    end
+  end
+
+  end
+
 end
