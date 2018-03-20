c29d03cffc75d077a9f2b96922d764da4f007262
Sally Zasloff <szasloff@gmail.com>
2018-03-20 21:31:01 +0000

    Done.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,12 @@
 class TransactionsController < ApplicationController
   use Rack::Flash
 
+
+  get '/transactions' do
+    @transactions = Transaction.all
+
+    erb :'/transactions/all_transactions'
+  end
+
+
 end
