10230bfad7b0bd3b6a4efd8b83f25bbe754b7630
Sally Zasloff <szasloff@gmail.com>
2018-03-20 16:32:47 +0000

    Done.

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -42,6 +42,57 @@ class CreditsController < ApplicationController
      end
    end
 
+   get '/credits/:id/edit' do
+     if logged_in?
+       @credit = Credit.find_by_id(params[:id])
+       if @credit.user_id == current_user.id
+         erb :'/credits/edit_credit'
+       end
+     else
+       redirect '/login'
+     end
+   end
+
+   patch '/credits/id' do
+     if logged_in?
+        if params[:credit_name]== ""|| params[:sector] == "" || params[:rating] == ""
+          redirect "/credits/#{params[:id]}/edit"
+        else
+          @credit = Credit.find_by_id(params[:id])
+            if @credit.user_id == current_user.id
+              if @credit.update(credit_name: params[:credit_name], sector: params[:sector], rating: params[:rating])
+                redirect "/credits/#{@credit.id}"
+              else redirect "/credits/#{@credit.id}/edit"
+              end
+          else
+            redirect '/credits'
+          end
+        end
+      else
+        redirect '/login'
+      end
+   end
+
+   delete '/credits/:id/delete' do
+     if logged_in?
+       @credit = Credit.find_by_id(params[:id])
+        if @credit.user_id == current_user.id
+          @credit.delete
+          redirect '/credits'
+        else
+          redirect '/login'
+        end
+      end
+    end
+
+  end
+
+
+
+
+   end
+
+
 
 
  end
