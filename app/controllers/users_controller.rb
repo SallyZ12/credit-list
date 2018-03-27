f26e638ebbd03c29c448325a417f8fd0490f7d14
Sally Zasloff <szasloff@gmail.com>
2018-03-27 22:14:32 +0000

    formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,6 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
